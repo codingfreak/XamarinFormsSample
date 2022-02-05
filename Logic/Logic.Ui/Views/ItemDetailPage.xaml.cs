52004f2db305b9e67f4b44e096554b2f74e625cb
Alexander Schmidt <alexander.schmidt@devdeer.com>
2022-02-05 20:40:49 +0100

    folder structure cleared

---
+++ Logic/Logic.Ui/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Ui.Mobile.ViewModels;
+using Xamarin.Forms;
+
+namespace Ui.Mobile.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
